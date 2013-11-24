4098c0875f881360c87821ca996827b74653d72c
Kurt Nelson <kurtisnelson@gmail.com>
2013-11-24 13:47:16 -0500

    Change manifest and use my activity

---
+++ src/com/thisisnotajoke/connectshades/HostView.java
@@ -0,0 +1,16 @@
+package com.thisisnotajoke.connectshades;
+
+import android.content.Context;
+
+import com.google.android.glass.widget.CardScrollView;
+
+public class HostView extends CardScrollView {
+
+	/**
+	 * @param context
+	 */
+	public HostView(Context context) {
+		super(context);
+	}
+
+}
